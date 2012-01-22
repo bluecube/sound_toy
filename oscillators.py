32747d8f76bd73409d38fc1360b2207fcda58ee3
Kuba Marek <blue.cube@seznam.cz>
2012-01-22 22:26:36 +0100

    Faster(?) oscillators.

--- oscillators.py
+++ oscillators.py
@@ -20,11 +20,13 @@ class Oscillator(BaseTrack):
             self.add_slave(phase)
 
         if amplitudeLow is None and amplitudeHigh is None:
-            self._amplitudeHigh = amplitude
-            self._amplitudeLow = _Negate(amplitude)
+            self._amplitude = amplitude
+            self._amplitudeHigh = None
+            self._amplitudeLow = None
             if isinstance(amplitude, BaseTrack):
                 self.add_slave(amplitude)
         elif amplitudeLow is not None and amplitudeHigh is not None:
+            self._amplitude = None
             self._amplitudeHigh = amplitudeHigh
             self._amplitudeLow = amplitudeLow
             if isinstance(amplitudeLow, BaseTrack):
@@ -36,39 +38,79 @@ class Oscillator(BaseTrack):
 
     def _func(self, x):
         raise NotImplemented()
-    
+
     @staticmethod
     def _make_iter(x):
-        if isinstance(x, _Negate):
-            x = x.track
-            if isinstance(x, BaseTrack):
-                return (-value for value in x)
-            else:
-                return itertools.repeat(-float(x))
-        if isinstance(x, BaseTrack):
-            return iter(x)
+        if Oscillator._is_constant(x):
+            return itertools.repeat(x)
         else:
-            return itertools.repeat(float(x))
+            return iter(x)
 
     @staticmethod
-    def _cumsum(it):
-        accumulator = 0
+    def _convert(x):
+        if x is None:
+            return None
+        elif isinstance(x, BaseTrack):
+            return iter(x)
+        else:
+            return float(x)
+
+    @staticmethod
+    def _cumsum(it, add = 0, multiplier = 1):
+        accumulator = add
         for x in it:
             yield accumulator
-            accumulator += x
+            accumulator += x * multiplier
+
+    def _is_constant(x):
+        return not hasattr(x, '__next__')
 
     def __iter__(self):
-        # TODO: some performance hides in here
+        freq = self._convert(self._freq)
+        phase = self._convert(self._phase)
+        amplitude = self._convert(self._amplitude)
+        amplitudeHigh = self._convert(self._amplitudeHigh)
+        amplitudeLow = self._convert(self._amplitudeLow)
+
+        freq_multiplier = 2 * math.pi / self._samplerate
+
+        if Oscillator._is_constant(freq):
+            if Oscillator._is_constant(phase):
+                x_iterator = itertools.count(phase, freq * freq_multiplier)
+            else:
+                x_iterator = itertools.count(0, freq * freq_multiplier)
+        else:
+            if Oscillator._is_constant(phase):
+                x_iterator = self._cumsum(freq, phase, freq_multiplier)
+            else:
+                x_iterator = self._cumsum(freq, 0, freq_multiplier)
+
+
+        if Oscillator._is_constant(phase):
+            if amplitude is not None:
+                if Oscillator._is_constant(amplitude):
+                    return (amplitude * self._func(x) for x in x_iterator)
+                else:
+                    return (a * self._func(x) for x, a in zip(x_iterator, amplitude))
+        else:
+            if amplitude is not None:
+                if Oscillator._is_constant(amplitude):
+                    return (amplitude * self._func(x + p) for x, p in zip(x_iterator, phase))
+                else:
+                    return (a * self._func(x + p) for x, a, p in zip(x_iterator, amplitude, phase))
+
+
+        assert amplitude is None
+
         zipped = zip(
-            self._cumsum(self._make_iter(self._freq)),
-            self._make_iter(self._phase),
-            self._make_iter(self._amplitudeLow),
-            self._make_iter(self._amplitudeHigh)
-            )
-        
-        return (amplitudeLow + (amplitudeHigh - amplitudeLow) * (0.5 + 0.5 * 
-            self._func(2 * math.pi * freq_integral / self._samplerate + phase)) for
-            (freq_integral, phase, amplitudeLow, amplitudeHigh) in zipped)
+            self._cumsum(self._make_iter(freq), 0, freq_multiplier),
+            self._make_iter(amplitudeHigh),
+            self._make_iter(amplitudeLow),
+            self._make_iter(phase))
+
+        return (((ah + al) + (ah - al) *
+            self._func(x + p)) * 0.5 for
+            x, ah, al, p in zipped)
 
 
 class SineOscillator(Oscillator):
