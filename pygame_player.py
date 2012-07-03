9505674e7dfcc25a81294ef6cb99cf2a78eca53a
Kuba Marek <blue.cube@seznam.cz>
2012-07-03 21:50:26 +0200

    Updating pygame player.

--- pygame_player.py
+++ pygame_player.py
@@ -7,55 +7,38 @@ import struct
 import itertools
 import math
 
-def _prepare_block(track, maximum, blocksize):
-    arr = numpy.zeros(blocksize, dtype=numpy.int16)
+_maximum = 2**15 - 1
 
-    last = None
+def _next_block(it):
+    values = next(it)
+    numpy.clip(values, -1, 1, out=values)
+    values *= _maximum
+    values = numpy.array(values, numpy.int16)
+    #print(values)
 
-    for i, val in enumerate(itertools.islice(track, blocksize)):
-        arr[i] = max(-maximum, min(maximum, maximum * val))
-        last = i
-    
-    if last is None:
-        return None
-    elif last != blocksize - 1:
-        numpy.resize(arr, last + 1)
-    
-    return pygame.sndarray.make_sound(arr)
+    return pygame.sndarray.make_sound(values)
 
 def play(track, samplerate=44100, blocksize=4096):
-    track.set_samplerate(samplerate)
-
     pygame.mixer.init(samplerate, -16, 1, blocksize)
-    pygame.display.init()
-    maximum = 2**15 - 1
-
-    it = iter(track)
+    pygame.display.init() # Needed for events ...
 
     try:
         channel = pygame.mixer.Channel(0)
-
         channel.set_endevent(pygame.locals.USEREVENT)
 
-        channel.play(_prepare_block(it, maximum, blocksize))
-        sound = _prepare_block(it, maximum, blocksize)
-        if sound is not None:
-            channel.queue(sound)
+        it = track.as_arrays_iter(samplerate, blocksize, None)
 
-        while channel.get_busy():
+        channel.play(_next_block(it))
+        channel.queue(_next_block(it))
+
+        while True:
             event = pygame.event.wait()
-            if event.type != pygame.locals.USEREVENT:
-                continue
 
-            sound = _prepare_block(it, maximum, blocksize)
-            if sound is None:
-                break
+            if event.type == pygame.locals.USEREVENT:
+                channel.queue(_next_block(it))
 
-            channel.queue(sound)
-
-    except KeyboardInterrupt:
-        print("Keyboard interrupt.")
     except StopIteration:
-        pass
+        while channel.get_busy():
+            pygame.event.wait()
     finally:
         pygame.quit()
