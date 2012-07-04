6157eff3983025c78083eacff0c0e7ff3fde2c23
Kuba Marek <blue.cube@seznam.cz>
2012-07-04 09:47:25 +0200

    Explicitly closing the pcm stream

--- alsa.py
+++ alsa.py
@@ -10,10 +10,22 @@ def play(track, samplerate=44100, periodsize=512, card='default'):
         type=alsaaudio.PCM_PLAYBACK,
         mode=alsaaudio.PCM_NORMAL,
         card=card)
-    pcm.setrate(samplerate)
-    pcm.setchannels(1)
-    pcm.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
-    pcm.setperiodsize(periodsize)
+
+    try:
+        pcm.setrate(samplerate)
+        pcm.setchannels(1)
+        pcm.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+        pcm.setperiodsize(periodsize)
+
+        dtype = numpy.dtype('<f4')
+
+        for values in track.as_arrays_iter(samplerate, periodsize):
+            numpy.clip(values, -1, 1, out=values)
+            out = numpy.array(values, dtype);
+
+            pcm.write(out.tostring())
+    finally:
+        pcm.close();
 
     dtype = numpy.dtype('<f4')
 
