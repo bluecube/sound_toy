9f28feeac5c5d2f627b8dad5d14db8701659fbf5
Kuba Marek <blue.cube@seznam.cz>
2012-07-10 15:42:00 +0200

    simple manual test that plots oscillator waves

---
+++ tests/plot_basic_oscillators.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+
+import sound_toy
+
+waveforms = []
+waveforms.append(sound_toy.oscillators.SineOscillator(
+    1,
+    amplitude = sound_toy.envelopes.Box(1)))
+waveforms[-1].name = "Sine"
+
+waveforms.append(sound_toy.oscillators.SawtoothOscillator(
+    1,
+    amplitude = sound_toy.envelopes.Box(1)))
+waveforms[-1].name = "Sawtooth"
+
+waveforms.append(sound_toy.oscillators.SquareOscillator(
+    1,
+    amplitude = sound_toy.envelopes.Box(1)))
+waveforms[-1].name = "Square"
+
+waveforms.append(sound_toy.oscillators.TriangleOscillator(
+    1,
+    amplitude = sound_toy.envelopes.Box(1)))
+waveforms[-1].name = "Triangle"
+
+
+sound_toy.waveform_plot.plot(waveforms)
+
