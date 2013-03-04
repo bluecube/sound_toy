e3446eec05df6eb55b195f9feb34a94a05e9c29c
Kuba Marek <blue.cube@seznam.cz>
2013-03-04 16:23:41 +0100

    More robust imports

--- waveform_plot.py
+++ waveform_plot.py
@@ -1,19 +1,16 @@
-try:
-    import matplotlib.pyplot as plt
-except ImportError:
-    print("Matplotlib is not available")
-else:
-    def plot(tracks, samplerate = 44100):
-        try:
-            iter(tracks)
-        except TypeError:
-            tracks = [tracks]
+import matplotlib.pyplot as plt
 
-        for track in tracks:
-            fig = plt.figure()
-            plot = fig.add_subplot(1, 1, 1)
-            plot.set_title(track.name)
-            plot.set_xlabel("time [s]")
-            plot.plot(track.as_array(samplerate))
+def plot(tracks, samplerate = 100):
+    try:
+        iter(tracks)
+    except TypeError:
+        tracks = [tracks]
 
-        plt.show()
+    for track in tracks:
+        fig = plt.figure()
+        plot = fig.add_subplot(1, 1, 1)
+        plot.set_title(track.name)
+        plot.set_xlabel("time [s]")
+        plot.plot(track.as_array(samplerate))
+
+    plt.show()
